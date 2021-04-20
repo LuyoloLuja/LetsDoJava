f7506098263cb6dca0abde4511771fc3ac8da604
LuyoloLuja <luyolom19@gmail.com>
2021-04-20 12:26:36 +0200

    initial commit

---
+++ StartHere.java
@@ -0,0 +1,18 @@
+public class StartHere {
+    public static void main(String[] args) {
+
+//        if (args.length == 0){
+//            System.out.println("I am starting with Java!");
+//        } else {
+//            System.out.println(args[0] + " is starting with Java!");
+//        }
+        GreetMe greetMe = new GreetMe();
+        System.out.println(greetMe.greet("Luyolo"));
+    }
+
+    public static class GreetMe {
+        public String greet(String name) {
+            return "Hello " + name;
+        }
+    }
+}
